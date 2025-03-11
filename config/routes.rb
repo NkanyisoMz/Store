22635b01d3cfc60825dc028652d07c022f85fd5c
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-03-11 22:16:54 +0200

    Add unsubscribe links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
+
 
 
   # get "posts/index"
